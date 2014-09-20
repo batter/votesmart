4db0e4d396853c62997b210b64c12add87e9b3f0
Mark Steckel <mjs@fix.net>
2014-09-19 19:56:40 -0700

    Include Address.get_*() in the parallelizable requests

--- lib/vote_smart/address.rb
+++ lib/vote_smart/address.rb
@@ -8,33 +8,33 @@ module VoteSmart
     
     
     # Returns a campaign office's contact information
-    def self.get_campaign can_id
-      request("Address.getCampaign", "candidateId" => can_id)
+    def self.get_campaign can_id, &block
+      request("Address.getCampaign", {"candidateId" => can_id}, &block)
     end
     
     # Returns a campaign office's contact information
-    def self.get_campaign_web_address can_id
-      request("Address.getCampaignWebAddress", "candidateId" => can_id)
+    def self.get_campaign_web_address can_id, &block
+      request("Address.getCampaignWebAddress", {"candidateId" => can_id}, &block)
     end
     
     # Returns a campaign office's contact information
-    def self.get_campaign_by_election election_id
-      request("Address.getCampaignByElection", "electionId" => election_id)
+    def self.get_campaign_by_election election_id, &block
+      request("Address.getCampaignByElection", {"electionId" => election_id}, &block)
     end
     
     # Returns an incumbent office's contact information
-    def self.get_office candidate_id
-      request("Address.getOffice", "candidateId" => candidate_id)
+    def self.get_office candidate_id, &block
+      request("Address.getOffice", {"candidateId" => candidate_id}, &block)
     end
     
     # Returns an incumbent office's contact information
-    def self.get_office_web_address can_id
-      request("Address.getOfficeWebAddress", "candidateId" => can_id)
+    def self.get_office_web_address can_id, &block
+      request("Address.getOfficeWebAddress", {"candidateId" => can_id}, &block)
     end
     
     # Returns a (sometimes)list of offices that fit office_id and state_id
-    def self.get_office_by_office_state office_id, state_id = 'NA'
-      request("Address.getOfficeByOfficeState", "officeId" => office_id, "stateId" => state_id)
+    def self.get_office_by_office_state office_id, state_id = 'NA', &block
+      request("Address.getOfficeByOfficeState", {"officeId" => office_id, "stateId" => state_id}, &block)
     end
     
   end
